83f934e04e35429716d2d952025fafa92e43d2e4
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2023-01-31 14:24:36 -0600

    Add files via upload

---
+++ lg-logo.py
@@ -0,0 +1,27 @@
+from turtle import *
+
+color('#bf177f')
+dot(300)
+up()
+goto(0, 120)
+down()
+color('white')
+width(10)
+left(180)
+circle(120, 270)
+
+left(90)
+forward(80)
+
+up()
+goto(-40, 50)
+down()
+dot(30)
+
+up()
+goto(0, 60)
+down()
+left(90)
+forward(120)
+left(90)
+forward(40)
